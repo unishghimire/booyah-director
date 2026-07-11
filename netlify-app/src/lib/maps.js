92efdd6e56731f96b2066094f8792de0f181e2ca
Unish Ghimire <nex.unishghimire@gmail.com>
2026-07-11 11:52:54 +0000

    Auto-commit agent changes

---
+++ netlify-app/src/lib/maps.js
@@ -0,0 +1,11 @@
+export const MAP_IMAGES = {
+  'Bermuda':     'https://media.base44.com/images/public/6a50ece85c2fb6cfcec2b53f/0933b78c6_Free_Fire_Map_Bermuda_2023.png',
+  'Bermuda 2.0': 'https://media.base44.com/images/public/6a50ece85c2fb6cfcec2b53f/a977759c0_Map_FF_Bermuda_20_allmode.jpeg',
+  'Kalahari':    'https://media.base44.com/images/public/6a50ece85c2fb6cfcec2b53f/69ba6d32b_Map_FF_Kalahari_allmode.jpeg',
+  'Purgatory':   'https://media.base44.com/images/public/6a50ece85c2fb6cfcec2b53f/34de455d0_Map_FF_Purgatory_allmode.jpeg',
+  'Alpine':      'https://media.base44.com/images/public/6a50ece85c2fb6cfcec2b53f/e0f94182f_Map_FF_Alpine_allmode.jpeg',
+  'Nexterra':    'https://media.base44.com/images/public/6a50ece85c2fb6cfcec2b53f/0a8d25686_Map_FF_Nexterra_allmode.jpeg',
+  'Solara':      'https://media.base44.com/images/public/6a50ece85c2fb6cfcec2b53f/032d43772_1200px-Map_FF_Solara_allmode.jpg',
+};
+
+export const MAPS = Object.keys(MAP_IMAGES);
